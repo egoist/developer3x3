3b86ad305d8f27c04d23a0c864f642a9f2e7fb47
EGOIST <hi@egoist.dev>
2023-02-13 18:44:35 +0800

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import "tippy.js/dist/tippy.css"
 import "../css/tailwind.css"
+import { AnalyticsWrapper } from "../components/Analytics"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   )
 }
